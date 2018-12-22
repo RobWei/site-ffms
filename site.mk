662f9fd3e6ad477a0ee5217fd266da23a8a78202
RobWei <admin@robwei.me>
2018-12-22 21:24:58 +0100

    * Maked changes to v2018.1.x

--- site.mk
+++ site.mk
@@ -1,31 +1,34 @@
-GLUON_SITE_PACKAGES := \
-	ffpb-ebtables-net-rules \
-	gluon-alfred \
-	gluon-respondd\
-	gluon-autoupdater \
-	gluon-config-mode-autoupdater \
-	gluon-config-mode-contact-info \
-	gluon-config-mode-core \
-	gluon-config-mode-geo-location \
-	gluon-config-mode-hostname \
-	gluon-config-mode-mesh-vpn \
-	gluon-ebtables-filter-multicast \
-	gluon-ebtables-filter-ra-dhcp \
-	gluon-luci-admin \
-	gluon-luci-autoupdater \
-	gluon-luci-portconfig \
-	gluon-luci-private-wifi \
-	gluon-luci-wifi-config \
-	gluon-mesh-batman-adv-15 \
-	gluon-mesh-vpn-fastd \
-	gluon-next-node \
-	uradvd \
-	gluon-setup-mode \
-	gluon-status-page \
-	haveged \
-	iptables \
-	iwinfo
-	 
+## Freifunk Muensterland
 
-GLUON_PRIORITY ?= 3
+GLUON_FEATURES := \
+	autoupdater \
+	ebtables-filter-multicast \
+	ebtables-filter-ra-dhcp \
+	ebtables-limit-arp \
+	ebtables-source-filter \
+	mesh-batman-adv-15 \
+	mesh-vpn-tunneldigger \
+	radvd \
+	respondd \
+	status-page \
+	web-advanced \
+	web-wizard \
+        web-private-wifi \
+	ssid-changer \
+        rfkill-disable \
+	advancedstats \
+	config-mode-statistics
+
+GLUON_SITE_PACKAGES := haveged iwinfo iptables
+
+DEFAULT_GLUON_RELEASE := 2018.1+exp$(shell date '+%Y%m%d')
+
+GLUON_RELEASE ?= $(DEFAULT_GLUON_RELEASE)
+
+GLUON_PRIORITY ?= 0
+
+GLUON_REGION ?= eu
+
 GLUON_LANGS ?= en de
+
+##GLUON_WLAN_MESH := ibss
